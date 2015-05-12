d7613e7eee2fcd25dc411c94ad264b27d6c919b4
Nguyen Minh Duc <ducnguyen240588@gmail.com>
2015-05-12 15:43:00 +0700

    Layout signup 1

---
+++ Owloop/Classes/Controller/SignUp/OLSignUpViewController.m
@@ -0,0 +1,45 @@
+//
+//  OLSignUpViewController.m
+//  Owloop
+//
+//  Created by Nguyen Minh Duc on 5/12/15.
+//  Copyright (c) 2015 OL. All rights reserved.
+//
+
+#import "OLSignUpViewController.h"
+
+@interface OLSignUpViewController ()
+
+@end
+
+@implementation OLSignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = YES;
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    self.navigationController.navigationBarHidden = NO;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
